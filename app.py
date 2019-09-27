aceba0c66af1d3b507e2a0b786db69b321875f18
Jay <pskfry@gmail.com>
2019-09-26 21:30:44 -0400

    working POST method for scraped data, added scrapelog route with basic GET and POST methods

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
+import sys
 from flask_sqlalchemy import SQLAlchemy
 from flask import (jsonify, request, Response, abort, Flask, render_template)
 import json
 
 from sqlalchemy import inspect
+from datetime import datetime
 
 def object_as_dict(obj):
     return {c.key: getattr(obj, c.key)
@@ -14,7 +16,7 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
-from models import Char
+from models import Char, Scrape
 
 @app.route('/')
 def home():
@@ -32,17 +34,45 @@ def chars():
 @app.route('/char', methods=['GET','POST'])
 def char():
     if request.method == 'POST':
-        if request.is_json:
+        try:
             req = request.get_json()
             charList = req["charList"]
+
+            Char.query.delete()
+
             for char in charList:
-                newChar = Char(char["charName"], char["lastUpdated"])
+                newChar = Char(char["charName"], char["lastUpdated"], "", char["itemLevel"], char["guildName"])
                 db.session.add(newChar)
-            db.session.commit()
-            return Response(status=200)
-        else:
+
+            new_row_count = Char.query.count()
+
+            if new_row_count > 25:
+                db.session.commit()
+                return Response(status=200)
+            else:
+                db.session.rollback()
+                abort(400)
+        except:
             abort(400)
-        
+
+@app.route('/scrapelog', methods=['GET','POST'])
+def scrape_log():
+    if request.method == 'GET':
+        scrapeList = Scrape.query.all()
+        return json.dumps([object_as_dict(scrape) for scrape in scrapeList],default=str)
+
+    if request.method == 'POST':
+        try:
+            req = request.get_json()
+            newScrape = Scrape(req["scrapeStart"], req["scrapeEnd"], req["charCount"], req["minIlvl"], req["pagesScraped"])
+            print(newScrape, file=sys.stderr)
+
+            db.session.add(newScrape)
+            db.session.commit()
+
+            return Response(status=200)
+        except:
+            abort(400)
 
 @app.route('/char/<int:charId>')
 def show_char(charId):
--- models.py
+++ models.py
@@ -1,4 +1,7 @@
 from app import db
+from datetime import datetime
+from dateutil.parser import parse
+
 
 class Char(db.Model):
     __tablename__ = 'characters'
@@ -9,9 +12,32 @@ class Char(db.Model):
     guild = db.Column(db.String(50))
     last_update = db.Column(db.DateTime)
 
-    def __init__(self, char_name, last_update, char_class = "", item_level = 0, guild = ""):
+    def __init__(self, char_name, last_update, char_class="", item_level=0, guild=""):
         self.char_name = char_name
         self.char_class = char_class
         self.item_level = item_level
         self.guild = guild
         self.last_update = last_update
+
+
+class Scrape(db.Model):
+    __tablename__ = 'scrape_log'
+    id = db.Column(db.Integer, primary_key=True)
+    scrape_start = db.Column(db.DateTime)
+    scrape_end = db.Column(db.DateTime)
+    char_count = db.Column(db.Integer)
+    min_ilvl = db.Column(db.Float)
+    pages_scraped = db.Column(db.Integer)
+
+    def __init__(self, scrape_start, scrape_end, char_count=0, min_ilvl=0, pages_scraped=0):
+        scrape_start = parse(scrape_start)
+        scrape_end = parse(scrape_end)
+        self.scrape_start = scrape_start
+        self.scrape_end = scrape_end
+        self.char_count = char_count
+        self.min_ilvl = min_ilvl
+        self.pages_scraped = pages_scraped
+
+    def __repr__(self):
+        return "started at %s ended at %s char count %s min ilv %s, %s pages scraped" % (
+            self.scrape_start, self.scrape_end, self.char_count, self.min_ilvl, self.pages_scraped)
