f4b7d7149390ddab16322b9359e6a9a5d8946490
Jay <pskfry@gmail.com>
2019-09-28 12:31:55 -0400

    added scrape log, added login methods and jwt implementation

--- app.py
+++ app.py
@@ -1,48 +1,61 @@
 import os
 import sys
+import json
 from flask_sqlalchemy import SQLAlchemy
 from flask import (jsonify, request, Response, abort, Flask, render_template)
-import json
-
 from sqlalchemy import inspect
+from flask_jwt_extended import (JWTManager, create_access_token, jwt_required, get_jwt_identity, get_raw_jwt)
 from datetime import datetime
 
 def object_as_dict(obj):
     return {c.key: getattr(obj, c.key)
             for c in inspect(obj).mapper.column_attrs}
 
+
 app = Flask(__name__, template_folder="templates")
+
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
 
-from models import Char, Scrape
+db = SQLAlchemy(app)
+jwt = JWTManager(app)
+
+from models import Char, Scrape, User
+
 
 @app.route('/')
 def home():
-    """
-    index response
-    """
-    charList = Char.query.all()
-    return render_template('home.html', charList=charList)
+    return NotImplementedError
+
 
 @app.route('/chars')
+@jwt_required
 def chars():
-    charList = Char.query.all()
-    return json.dumps([object_as_dict(char) for char in charList],default=str)
+    page = 1 if request.args.get('page') is None else int(request.args.get('page'))
+    limit = 20 if request.args.get('limit') is None else int(request.args.get('limit'))
+
+    char_list = Char.query.paginate(page, limit, False).items
+    res_body = {"char_list": []}
+
+    res_body["char_list"].extend([object_as_dict(char) for char in char_list])
+    return json.dumps(res_body, default=str)
+
 
 @app.route('/char', methods=['GET','POST'])
+@jwt_required
 def char():
     if request.method == 'POST':
         try:
             req = request.get_json()
-            charList = req["charList"]
+            print(req, file=sys.stderr)
+            char_list = req["char_list"]
 
             Char.query.delete()
 
-            for char in charList:
-                newChar = Char(char["charName"], char["lastUpdated"], "", char["itemLevel"], char["guildName"])
-                db.session.add(newChar)
+            for char in char_list:
+                new_char = Char(char["char_name"], char["last_updated"], char["char_class"], char["item_level"], char["guild"])
+                db.session.add(new_char)
 
             new_row_count = Char.query.count()
 
@@ -51,39 +64,71 @@ def char():
                 return Response(status=200)
             else:
                 db.session.rollback()
-                abort(400)
+            return Response(status=400)
         except:
-            abort(400)
+            return Response(status=400)
+
 
 @app.route('/scrapelog', methods=['GET','POST'])
+@jwt_required
 def scrape_log():
     if request.method == 'GET':
-        scrapeList = Scrape.query.all()
-        return json.dumps([object_as_dict(scrape) for scrape in scrapeList],default=str)
+        scrape_list = Scrape.query.all()
+        return Response(response=json.dumps([object_as_dict(scrape) for scrape in scrape_list],default=str), status=200, mimetype="application/json")
 
     if request.method == 'POST':
         try:
             req = request.get_json()
-            newScrape = Scrape(req["scrapeStart"], req["scrapeEnd"], req["charCount"], req["minIlvl"], req["pagesScraped"])
-            print(newScrape, file=sys.stderr)
+            new_scrape = Scrape(
+                req["scrape_start"], req["scrape_end"], req["char_count"], 
+                req["min_ilvl"], req["pages_scraped"]
+            )
 
-            db.session.add(newScrape)
+            db.session.add(new_scrape)
             db.session.commit()
 
             return Response(status=200)
         except:
-            abort(400)
+            return Response(status=400)
 
-@app.route('/char/<int:charId>')
-def show_char(charId):
-    return 'here is the info for char with id %s' % charId
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/char/<int:char_id>')
+def show_char(char_id):
+    return 'here is the info for char with id %s' % char_id
+
+
+@app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        return 'logging ya in'
-    else:
-        return 'heres a login form'
+    try:
+        req = request.json
+        user = User.find_by_username(req["user_name"])
+
+        if not user:
+            return Response(response="get pooped on retard", status=400)
+
+        user_verified = user.verify_hash(req["password"], user.password)
+
+        if user_verified:
+            access_token = create_access_token(identity = req["user_name"])
+            res_body = {"access_token": access_token}
+            
+            return Response(response=json.dumps(res_body), mimetype="application/json", status=201)
+        else:
+            return Response(response="fuck u idiot", status=401)
+
+    except ValueError as err:
+        return Response(response="{0}".format(err), status=400)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    try:
+        req = request.get_json()
+        if User.find_by_username(req["user_name"]):
+            return Response(status_code=400)
+
+    except:
+        return Response(status_code=400)
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,6 +1,29 @@
+import sys
 from app import db
 from datetime import datetime
 from dateutil.parser import parse
+from passlib.hash import pbkdf2_sha256 as sha256
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    date_added = db.Column(db.DateTime)
+    last_login = db.Column(db.DateTime)
+    permissions = db.Column(db.Boolean, nullable=False, default=True)
+
+    @staticmethod
+    def generate_hash(password):
+        return sha256.hash(password)
+
+    @staticmethod
+    def verify_hash(password, hash):
+        return sha256.verify(password, hash)
+
+    @classmethod
+    def find_by_username(cls, username):
+        return cls.query.filter_by(user_name=username).first()
 
 
 class Char(db.Model):
